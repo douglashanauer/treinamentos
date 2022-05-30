e760c89d74c2b814ca99f98682b4ca0300871298
douglashanauer <106168490+douglashanauer@users.noreply.github.com>
2022-05-29 21:37:56 -0300

    Add files via upload

---
+++ Tabuada/Tabuada_1_ao_10.java
@@ -0,0 +1,10 @@
+public class Tabuada_1_ao_10
+{
+    public static void main (String args[]){
+        for(int num1 = 0; num1 <= 10; num1++){
+            for(int num2 = 0; num2 <= 10; num2++){
+                System.out.println(num1 + "x" + num2 + "=" + num1*num2);
+            }
+        }
+    }
+}
