703c3ebaed3f34d2d3831e5bfc40b02230da54b3
douglashanauer <106168490+douglashanauer@users.noreply.github.com>
2022-05-29 21:37:21 -0300

    Add files via upload

---
+++ Multiplos_de_5/Multiplicador.java
@@ -0,0 +1,12 @@
+
+
+public class Multiplicador
+{
+    public static void main(String args[]){
+        
+        for(int num1 = 0; num1 <= 10; num1++){
+            System.out.println(num1 + "x 5 = " + num1*5);
+        }
+    }
+    
+}
